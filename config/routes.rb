8360003a19f1f797071ca383d5a115ca1141c55f
nithin-hunt <nithinpsgice@gmail.com>
2023-01-21 22:17:21 +0530

    Add signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "about", to: "about#index" # "/about" also works, "/" optional  
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"  # get "/" to (also works)
 end
